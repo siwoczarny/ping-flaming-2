21e4fc99edc01e3b3625060fd48437f777420522
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-06-09 17:05:07 +0200

    Updates

---
+++ precache-manifest.e7dfba5776b9e17ca1fd7d26fbbbef6d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3f8c8aa97dc714287fcdadcecaa815",
+    "url": "/ping-flaming-2/index.html"
+  },
+  {
+    "revision": "ae1911478587fe381160",
+    "url": "/ping-flaming-2/static/css/2.36d93137.chunk.css"
+  },
+  {
+    "revision": "ae1911478587fe381160",
+    "url": "/ping-flaming-2/static/js/2.7f370c59.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/ping-flaming-2/static/js/2.7f370c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61b95d22025a1a17229",
+    "url": "/ping-flaming-2/static/js/main.d06614d9.chunk.js"
+  },
+  {
+    "revision": "af217dcc710a5a0fa975",
+    "url": "/ping-flaming-2/static/js/runtime-main.faa4f95d.js"
+  },
+  {
+    "revision": "8af2367e0b0ae1c4dc1e25e80197126b",
+    "url": "/ping-flaming-2/static/media/cross.8af2367e.svg"
+  },
+  {
+    "revision": "f129d7d881172462a61500a5ee5224d6",
+    "url": "/ping-flaming-2/static/media/delete.f129d7d8.svg"
+  },
+  {
+    "revision": "b7019e5156298262922de756a2513e9d",
+    "url": "/ping-flaming-2/static/media/down.b7019e51.svg"
+  },
+  {
+    "revision": "19f636a4e774bc2dba39db7158fd5cde",
+    "url": "/ping-flaming-2/static/media/edit.19f636a4.svg"
+  },
+  {
+    "revision": "07cfa315e163c3709baee2a631c9c38d",
+    "url": "/ping-flaming-2/static/media/flaming-1.07cfa315.svg"
+  },
+  {
+    "revision": "b11a296a46e5b1132d75d0ded520e09c",
+    "url": "/ping-flaming-2/static/media/flaming-2.b11a296a.svg"
+  },
+  {
+    "revision": "108933bcba0e04ca2556b90673e8c711",
+    "url": "/ping-flaming-2/static/media/left.108933bc.svg"
+  },
+  {
+    "revision": "f9ee298d2012ad35c7bfe9a4decb2fc9",
+    "url": "/ping-flaming-2/static/media/log-out.f9ee298d.svg"
+  },
+  {
+    "revision": "ec1c933ccf9f55d2af8edfd4d30a5048",
+    "url": "/ping-flaming-2/static/media/logo-name.ec1c933c.svg"
+  },
+  {
+    "revision": "59ffaaba4c62620ff7a3f6a93b9d280c",
+    "url": "/ping-flaming-2/static/media/logo.59ffaaba.svg"
+  },
+  {
+    "revision": "9073fb03bcb72be15788d00f9f5fcf8b",
+    "url": "/ping-flaming-2/static/media/magnifier.9073fb03.svg"
+  },
+  {
+    "revision": "ab3b4cfa163847dc59c4ebd838faff11",
+    "url": "/ping-flaming-2/static/media/more.ab3b4cfa.svg"
+  },
+  {
+    "revision": "b7e7e40914073caf01f2ec33799accce",
+    "url": "/ping-flaming-2/static/media/plus.b7e7e409.svg"
+  },
+  {
+    "revision": "47c165070e02f359aa5e84753efaa1d6",
+    "url": "/ping-flaming-2/static/media/rectangle.47c16507.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ping-flaming-2/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ping-flaming-2/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ping-flaming-2/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "0ff702d87a5da1f651dae6f8679cac8a",
+    "url": "/ping-flaming-2/static/media/right.0ff702d8.svg"
+  },
+  {
+    "revision": "cc595825dde291638e818bc5505c9114",
+    "url": "/ping-flaming-2/static/media/show.cc595825.svg"
+  }
+]);
